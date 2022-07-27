f664b60fb23d71a24827aba9cec7a405918de564
GabiProg <gabrielaterese07@gmail.com>
2022-07-27 14:59:49 -0300

    Filmes renderizados

--- src/components/MovieGallery.js
+++ src/components/MovieGallery.js
@@ -1,3 +1,51 @@
-export default function MovieGallery(){
-    return(<>GALERIA DE FILMES</>);
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "../styles/Reset.css";
+import "../styles/Style_gallery.css";
+
+function Posters({ source, alt }) {
+    return (
+        <>
+            <div className="filmBox">
+                <img src={source} alt={alt} />
+            </div>
+        </>
+    );
+}
+
+export default function MovieGallery() {
+
+    const [images, setImages] = useState(
+        [
+            {
+                id: null,
+                source: null,
+                alt: null
+            }
+        ]);
+
+    useEffect(() => {
+        const URL = 'https://mock-api.driven.com.br/api/v5/cineflex/movies';
+
+        const promise = axios.get(URL);
+
+        promise.then(response => {
+            setImages([...response.data]);
+        });
+    }, []);
+
+    return (
+        <div className="screen">
+            <div className="select">
+                <h2>Selecione o filme</h2>
+            </div>
+            <div className="gallery">
+                {images.map(image => <Posters
+                    key={image.id}
+                    source={image.posterURL}
+                    alt={image.title}
+                    id={image.id} />)}
+            </div>
+        </div>
+    );
 }
